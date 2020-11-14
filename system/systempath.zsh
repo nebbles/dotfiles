326d3c5742c3860d3279d326a92c0dee8e5e01ec
nebbles <bsdgreenberg@gmail.com>
2020-11-14 23:42:13 +0000

    fix(system): Update system PATH to priotise repo
    The system PATH has been changed to prioritise the bin executables in
    the dotfiles repo over everything else installed (except for the local
    bin directory of the current working directory.

--- system/systempath.zsh
+++ system/systempath.zsh
@@ -2,11 +2,12 @@
 # after the other path modifying scripts, but will still be loaded before
 # oh-my-zsh.
 
-# Paths to add
-#   1. ./bin the bin folder of current working directory (for project folders)
-#   2.
-#   3.
-#   4. $DOTFILES/bin to add this dotfiles binaries to path
+# Paths to add (the order determines discovery order when calling executable)
+#
+#   1. ./bin            the bin folder local to any cwd (e.g. for projects)
+#   2. $DOTFILES/bin    the bin executables of this dotfiles repo
+#   3. /usr/local/bin
+#   4. /usr/local/sbin
 #   5. the rest...
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="./bin:$DOTFILES/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
