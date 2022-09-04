f700f14897897c625ec4279771666bdb26a55400
nebbles <github@greenberg.io>
2022-09-04 22:30:55 +0100

    feat(alias): Add ggraph special command for git
    The new ggraph alias is similar to glog but better formated. Credit and
    thanks to @Benedict-Carling

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,6 +2,8 @@
 
 alias glog='git log --graph --oneline --color --decorate --all'
 
+alias ggraph="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
+
 alias gpull='((git checkout develop && git pull origin develop) || true) && git checkout master && git pull origin master'
 
 alias gfetch='BRANCH=$(git rev-parse --abbrev-ref HEAD) && git fetch --all --prune && git stash && gitpull && git checkout $BRANCH && git stash pop'
