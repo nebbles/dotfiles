3461451a8062766eb7aee6d9f464b8d5f73830ef
nebbles <github@greenberg.io>
2022-09-13 14:40:06 +0100

    feat(git): Add SSH_AUTH_SOCK env variable

---
+++ git/ssh.zsh
@@ -0,0 +1,4 @@
+# Configure SSH auth socket to use 1Password
+# References: 
+# - https://1password.community/discussion/132867/visual-studio-code-remote-development-server-and-1password-ssh-config
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
