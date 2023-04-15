d85f31f02cef857f60f5a6bf96c41da077bec754
nebbles <github@greenberg.io>
2023-04-15 20:16:42 +0100

    feat(op): Add blank 1password file

---
+++ 1password/op.zsh
@@ -0,0 +1,8 @@
+# This loads the 1password-cli plugin confiuration into the shell environment,
+# enabling it to automatically alias common commands and provide the appropriate
+# credentials directly from 1Password. It does this by providing them as
+# environment variables to sub processes.
+# https://developer.1password.com/docs/cli/get-started#install
+# https://blog.1password.com/1password-cli-2_0/
+
+#source /Users/bengreenberg/.config/op/plugins.sh
